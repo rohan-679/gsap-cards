8dddb749337efebee06ad436090cced13c7fecd3
rohan-679 <76933542+rohan-679@users.noreply.github.com>
2021-08-16 09:56:06 +0530

    kira

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+
+gsap.from('.left',{ x:'-100%', duration : 2,ease: Power1. easeOut,opacity:0})
+gsap.from('.right',{ y:'-100%', duration : 3,ease: Power1. easeOut,opacity:0})
+gsap.from('.socials',{ x:'-100%', duration : 3,ease: Power1. easeOut, stagger:.1})
+gsap.from('.year',{ x:'-100%', duration : 3,ease: Power1. easeOut, stagger:.1})
